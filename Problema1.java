16ac57dc28a9f8b781b78bb7ffa51a642a381ab6
dixko123 <42257229+dixko123@users.noreply.github.com>
2018-08-12 15:03:28 -0500

    Create Problema1.java

---
+++ Problema1.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class Problema1<E> extends LinkedList<E> {
+	public String palabra="google";
+	
+	public char buscaRepetido(){	
+		int cuentaLetras=0,
+			contador=0;
+		char busca = 0;
+		
+		while(cuentaLetras!=1) {
+			busca=this.palabra.charAt(contador++);
+			cuentaLetras=0;
+			for (int i = 0; i < this.palabra.length(); i++) {
+				if(busca==this.palabra.charAt(i)) {					
+					cuentaLetras++;
+				}
+			}
+		}
+		System.out.println(busca);
+		return busca;
+		
+	}
+	public static void main(String[] args) {
+		Problema1<String> p = new Problema1<>();
+		p.buscaRepetido();
+	}
+}
